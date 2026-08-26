e62916b6586b0d514b71548ee5486db111917560
PratyushS2004 <pratyushs2004@gmail.com>
2026-08-26 13:08:58 -0400

    Initial commit

---
+++ Core/main.c
@@ -0,0 +1,33 @@
+#include <stdint.h>
+#include "stm32f446xx.h"
+
+int main(void)
+{
+
+  // Enable clock
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+  RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+
+  // Enable PA2/3 for TX/RX to alternate function mode
+  GPIOA->MODER |= (2 << 4) | (2 << 6);
+
+  // Enable AF7 (USART2) in PA2/3
+  GPIOA->AFR[0] |= (7 << 8) | (7 << 12);
+  
+  // USART Config
+  USART2->CR1 |= USART_CR1_UE;     // Enable USART
+  USART2->CR1 &= ~USART_CR1_M;     // 8 data bits
+  USART2->CR2 &= ~USART_CR2_STOP;  // 1 stop bit
+  USART2->BRR = (8 << 4) | 11;     // 115200 baud & 16MHz
+  USART2->CR1 |= USART_CR1_TE | USART_CR1_RE; // Enable TX, RX
+
+  /* Infinite loop */
+  while (1)
+  {
+
+
+  }
+}
+
+
+
