68c232a75dbc55ec72d6fa637c1bf7f295bc4d63
PratyushS2004 <pratyushs2004@gmail.com>
2026-09-05 14:10:05 -0400

    Add sector table and last_sector lookup

--- Bootloader/flash.c
+++ Bootloader/flash.c
@@ -3,6 +3,38 @@
 uint8_t erase_single_sector(uint8_t sector);
 uint8_t clear_BSY(void);
 uint8_t sector_erase(uint8_t last_sector);
+uint8_t last_sector(uint32_t length);
+
+typedef struct 
+{
+    uint8_t sector;   // Sector number
+    uint32_t address; // Sector start address
+}flash_sector;
+
+static const flash_sector sector_table[] = {
+    {2, 0x08008000}, // Sector 2, 16K
+    {3, 0x0800C000}, // Sector 3, 16K
+    {4, 0x08010000}, // Sector 4, 64K
+    {5, 0x08020000}, // Sector 5, 128K
+    {6, 0x08040000}, // Sector 6, 128K
+    {7, 0x08060000}  // Sector 7, 128K
+};
+
+/* 
+Given the lengh, find the last sector the payload covers
+*/
+uint8_t last_sector(uint32_t length){
+    uint8_t sector = 100; //Invalid Sector
+    uint32_t end_address = sector_table[0].address + length - 1;
+    const uint8_t total_sectors = 6;
+
+    for(uint8_t i = 0; i < total_sectors; i++){
+        if(end_address >= sector_table[i].address){
+            sector = sector_table[i].sector;
+        }
+    }
+    return sector;
+}
 
 /*
 Erases sector.
@@ -13,7 +45,7 @@ uint8_t erase_single_sector(uint8_t sector){
         FLASH->CR |= FLASH_CR_SER;  // Sector Erase Activated
         FLASH->CR |= sector << FLASH_CR_SNB_Pos; // Select sector to erase
         FLASH->CR |= FLASH_CR_STRT; // Trigger erase operation
-        if(wait_BSY()){
+        if(clear_BSY()){
             if(FLASH->SR & FLASH_SR_WRPERR){ // Write protection error
                 return 0;
             }
@@ -21,7 +53,7 @@ uint8_t erase_single_sector(uint8_t sector){
     return 1;
 }
 
-uint8_t wait_BSY(void) {
+uint8_t clear_BSY(void) {
     uint32_t timer = 10000000;
     while(FLASH->SR & FLASH_SR_BSY){ // Wait for BSY to clear
         if(--timer == 0){
@@ -32,6 +64,9 @@ uint8_t wait_BSY(void) {
 }
 
 uint8_t sector_erase(uint8_t last_sector){
+    if(last_sector > 7 || last_sector < 2){ // Invalid sector
+        return 0;
+    }
     uint8_t erase_ok = 1;
 
     if(FLASH->CR & FLASH_CR_LOCK){
@@ -40,7 +75,7 @@ uint8_t sector_erase(uint8_t last_sector){
     FLASH->KEYR = 0xCDEF89AB; // KEY2
     }
 
-    if(wait_BSY()){
+    if(clear_BSY()){
         for( uint8_t sector = 2; sector <= last_sector; sector++){
             if(erase_single_sector(sector) == 0){ // erase helper threw an error
                erase_ok = 0;  
