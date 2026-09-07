4aa3f9cb62dee5b328f2db3238f11ae8105ac2bc
PratyushS2004 <pratyushs2004@gmail.com>
2026-09-07 15:40:07 -0400

    flash: add program_word/flash_init. fixed single sector erase logic and flag clears

--- Bootloader/flash.c
+++ Bootloader/flash.c
@@ -3,7 +3,7 @@
 #include "stm32f446xx.h"
 
 uint8_t erase_single_sector(uint8_t sector);
-uint8_t clear_BSY(void);
+uint8_t wait_BSY(void);
 uint8_t sector_erase(uint8_t last_sector);
 uint8_t last_sector(uint32_t length);
 
@@ -26,6 +26,10 @@ static const flash_sector sector_table[] = {
 Given the lengh, find the last sector the payload covers
 */
 uint8_t last_sector(uint32_t length){
+    if (length == 0) {
+        return 100; // Invalid Sector
+    }
+
     uint8_t sector = 100; //Invalid Sector
     uint32_t end_address = sector_table[0].address + length - 1;
     const uint8_t total_sectors = 6;
@@ -43,19 +47,27 @@ Erases sector.
 Returns 1 for its successful or 0 if not.
 */
 uint8_t erase_single_sector(uint8_t sector){
-        FLASH->CR &= ~FLASH_CR_SNB; // Clear SNB
-        FLASH->CR |= FLASH_CR_SER;  // Sector Erase Activated
-        FLASH->CR |= sector << FLASH_CR_SNB_Pos; // Select sector to erase
-        FLASH->CR |= FLASH_CR_STRT; // Trigger erase operation
-        if(clear_BSY()){
-            if(FLASH->SR & FLASH_SR_WRPERR){ // Write protection error
-                return 0;
-            }
-        }else{ return 0; }// BSY timeout }
+    FLASH->CR &= ~FLASH_CR_SNB; // Clear SNB
+    FLASH->CR |= FLASH_CR_SER;  // Sector Erase Activated
+    FLASH->CR |= sector << FLASH_CR_SNB_Pos; // Select sector to erase
+    FLASH->CR |= FLASH_CR_STRT; // Trigger erase operation
+
+    uint8_t bsy_ok = wait_BSY();
+    FLASH->CR &= ~FLASH_CR_SER; // Clear SER
+
+    if(!bsy_ok){ // Check BSY status
+        return 0;
+    }
+    
+    if(FLASH->SR & FLASH_SR_WRPERR){ // Write protection error
+        FLASH->SR |= FLASH_SR_WRPERR; // Clear flag
+        return 0; 
+    }
+
     return 1;
 }
 
-uint8_t clear_BSY(void) {
+uint8_t wait_BSY(void) {
     uint32_t timer = 3000;
     uint32_t start = ms_ticks;
 
@@ -79,9 +91,9 @@ uint8_t sector_erase(uint8_t last_sector){
     FLASH->KEYR = 0xCDEF89AB; // KEY2
     }
 
-    if(clear_BSY()){
+    if(wait_BSY()){
         for( uint8_t sector = 2; sector <= last_sector; sector++){
-            if(erase_single_sector(sector) == 0){ // erase helper threw an error
+            if(erase_single_sector(sector) == 0){ // Erase helper threw an error
                erase_ok = 0;  
                break;
             }
@@ -90,6 +102,42 @@ uint8_t sector_erase(uint8_t last_sector){
         erase_ok = 0;
     }
     
-    FLASH->CR |= FLASH_CR_LOCK; // Lock
+    if(wait_BSY()){
+        FLASH->CR |= FLASH_CR_LOCK; // Lock
+    }
+
     return erase_ok;
+}
+
+void flash_init(void) {
+    // Lock in 32-bit parallelism for 3.3V board rail
+    FLASH->CR &= ~FLASH_CR_PSIZE;
+    FLASH->CR |= FLASH_CR_PSIZE_1;
+}
+
+uint8_t program_word(uint32_t address, uint32_t data){
+    if (address % 4 != 0) { // Checks 32-bit (4-byte) alignment
+        return 0;
+    }
+
+    if(!wait_BSY()){ // Check BSY status
+        return 0;
+    }
+
+    FLASH->CR |= FLASH_CR_PG; // Set Programming
+    *(uint32_t*) address = data; // Push data into address
+    
+    uint8_t bsy_ok = wait_BSY(); 
+    FLASH->CR &= ~FLASH_CR_PG; // Clear PG
+
+    if(!bsy_ok){ // Check BSY status
+        return 0;
+    }
+
+    if (FLASH->SR & (FLASH_SR_PGAERR | FLASH_SR_PGPERR | FLASH_SR_PGSERR)){ // Check status flags
+        FLASH->SR |= FLASH_SR_PGAERR | FLASH_SR_PGPERR | FLASH_SR_PGSERR; // Clear status flags
+        return 0;
+    }
+
+    return 1;
 }
