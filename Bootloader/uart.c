62676c4881a137fe83d82de2463ca5ba06dfa4ed
PratyushS2004 <pratyushs2004@gmail.com>
2026-09-13 23:17:36 -0400

    Received total length and chunk length logic

--- Bootloader/uart.c
+++ Bootloader/uart.c
@@ -1,8 +1,10 @@
 #include <stdint.h>
 #include "stm32f446xx.h"
 #include <stdbool.h>
+#include "main.h"
 
 #define CHUNK_PAYLOAD_SIZE 1024 // 1KB chunk payload
+#define MAX_PAYLOAD_SIZE 491520 // 480KB
 #define ACK 0x06U
 #define NAK 0x15U
 
@@ -16,27 +18,34 @@ typedef enum{
 typedef struct{
   Boot_state state;
 
-  uint8_t word_buf[4];
-  uint8_t byte_count;
+  uint8_t word_buf[4]; // 4-byte assembler for program_word()
+  uint8_t byte_count;  // Assembler index (0 to 3)
 
-  uint32_t total_length;
-  uint32_t total_bytes_rx;
+  uint32_t total_length;   // Upfront total image size in bytes
+  uint32_t total_bytes_rx; // Cumulative bytes processed across all chunks
 
-  uint32_t expected_chunk_length;
-  uint32_t chunk_bytes_rx;
+  uint32_t current_chunk_length; // Expected size of active chunk
+  uint32_t chunk_bytes_rx;        // Bytes received for current active chunk
 
-  uint8_t  chunk_buffer[CHUNK_PAYLOAD_SIZE];
+  uint8_t chunk_buffer[CHUNK_PAYLOAD_SIZE]; // 1KB buffer for CRC verification
 
-  uint32_t received_CRC;
-  uint32_t calculated_CRC;
+  uint32_t received_CRC;   // CRC read from the wire
+  uint32_t calculated_CRC; // Calcualted CRC
 } Boot_context;
 
-
-bool assemble_byte(Boot_context *context, uint8_t data, uint32_t *word){
-  context->word_buf[context->byte_count] = data;
+/**
+ * @brief Assembles incoming bytes into a 4-byte buffer.
+ * @param context Pointer to the bootloader context.
+ * @param byte The incoming single byte from UART.
+ * @param word Pointer to a uint32_t to store the assembled result.
+ * @return true (1) if 4 bytes have been fully assembled; false (0) if still assembling.
+ */
+bool assemble_byte(Boot_context *context, uint8_t byte, uint32_t *word){
+  context->word_buf[context->byte_count] = byte;
   context->byte_count++;
 
-  if(context->byte_count == 4){
+  if(context->byte_count == 4){ 
+    // Little-Endian transfer
     *word = ((uint32_t)context->word_buf[0]) | ((uint32_t)context->word_buf[1] << 8) |
                 ((uint32_t)context->word_buf[2] << 16) | ((uint32_t)context->word_buf[3] << 24);
 
@@ -47,9 +56,59 @@ bool assemble_byte(Boot_context *context, uint8_t data, uint32_t *word){
   return false; 
 }
 
-int main(void)
-{
+void state_machine(void)
+{ 
+  Boot_context context;
+  while (1)
+  {
+    switch(context.state){
+      case IDLE:{
+        // Waiting for initial start command byte
+        if((USART2->SR & USART_SR_RXNE) != 0){
+          context.byte_count = 0;
+          context.state = RX_TOTAL_LENGTH;
+        }
+        break;
+      }
+      case RX_TOTAL_LENGTH:{
+        // Accumulate 4 bytes for total image length
+        uint8_t rx_byte = receiveChar();
+        if (assemble_byte(&context, rx_byte, &context.total_length)) {
+            if(context.total_length >= MAX_PAYLOAD_SIZE){
+              context.state = ERROR;
+            }else{
+              erase_for_length(context.total_length);
+              context.state = RX_CHUNK_LENGTH;
+            }
+        }
+        break;
+      }
+      case RX_CHUNK_LENGTH:{
+        uint8_t rx_byte = receiveChar();
+        if (assemble_byte(&context, rx_byte, &context.current_chunk_length)) {
+            if(context.current_chunk_length > CHUNK_PAYLOAD_SIZE || 
+              context.current_chunk_length > (context.total_length - context.total_bytes_rx )){
+              context.state = ERROR;
+            }else{
+              context.state = RX_CHUNK_PAYLOAD;
+            }
+        }
+        break;
+      }
+      
+    
+    
+    }
 
+
+
+
+
+
+  }
+}
+
+void UART_Config(void){
   // Enable clock
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
   RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
@@ -67,24 +126,6 @@ int main(void)
   USART2->BRR = (8 << 4) | 11;     // 115200 baud & 16MHz
   USART2->CR1 |= USART_CR1_TE | USART_CR1_RE | USART_CR1_UE; // Enable TX, RX, USART
 
-  Boot_context context;
-  while (1)
-  {
-    switch(context.state){
-      case IDLE:
-        if((USART2->SR & USART_SR_RXNE) == 0){
-          context.byte_count = 0;
-          context.state = RX_TOTAL_LENGTH;
-        }
-        break;
-      case RX_TOTAL_LENGTH:
-        
-    }
-
-
-
-
-  }
 }
 
 void sendChar (uint8_t c){
